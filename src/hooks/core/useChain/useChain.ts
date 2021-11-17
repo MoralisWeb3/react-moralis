fe74e06adc0f05d9f522c24f8f89ef1d8c5b160a
Erno <erno@ivan.academy>
2021-11-17 15:48:13 +0000

    fix: error logic in useChain

--- src/hooks/core/useChain/useChain.ts
+++ src/hooks/core/useChain/useChain.ts
@@ -1,44 +1,47 @@
+import { useMemo } from "react";
 import { getChain } from "../../../functions/chains";
 import { useMoralis } from "../useMoralis";
 
 export const useChain = () => {
-  const { Moralis, isWeb3Enabled, enableWeb3, chainId, account } = useMoralis();
+  const { Moralis, chainId, account } = useMoralis();
 
-  async function switchNetwork(providedChainId: string) {
-    if (isWeb3Enabled) {
-      try {
-        await Moralis.Web3.switchNetwork(providedChainId);
-      } catch (error) {
-        if (error.code === 4902) {
-          try {
-            const chainData = getChain(providedChainId);
+  const switchNetwork = async (providedChainId: string) => {
+    try {
+      await Moralis.Web3.switchNetwork(providedChainId);
+    } catch (error) {
+      if (error.code === 4902) {
+        const chainData = getChain(providedChainId);
 
-            if (!chainData) {
-              throw new Error(
-                `Chain ${providedChainId} not supported or is not specified`,
-              );
-            }
-            const { chainId, name, nativeCurrency, rpc, blockExplorerUrl } =
-              chainData;
-
-            await Moralis.Web3.addNetwork(
-              chainId,
-              name,
-              nativeCurrency.name,
-              nativeCurrency.symbol,
-              rpc[0],
-              blockExplorerUrl ?? "",
-            );
-          } catch (error) {
-            alert(error.message);
-          }
+        if (!chainData) {
+          throw new Error(
+            `Chain ${providedChainId} not supported or is not specified`,
+          );
         }
+        const { chainId, name, nativeCurrency, rpc, blockExplorerUrl } =
+          chainData;
+
+        await Moralis.Web3.addNetwork(
+          chainId,
+          name,
+          nativeCurrency.name,
+          nativeCurrency.symbol,
+          rpc[0],
+          blockExplorerUrl ?? "",
+        );
+      } else {
+        throw error;
       }
-    } else {
-      enableWeb3();
     }
-  }
-  return { switchNetwork, chainId, account };
+  };
+
+  const chain = useMemo(() => {
+    if (!chainId) {
+      return null;
+    }
+    return getChain(chainId);
+  }, [chainId]);
+
+  return { switchNetwork, chainId, chain, account };
 };
 
 export default useChain;
