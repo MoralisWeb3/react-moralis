a0b20904e72170a3dda0fcc684c21a974ce2b227
Dmitry Kapeliushnyi <78314301+Y0moo@users.noreply.github.com>
2021-11-27 19:59:23 +0200

    Update useOneInchQuote.ts

--- src/hooks/plugins/useOneInch/useOneInchQuote.ts
+++ src/hooks/plugins/useOneInch/useOneInchQuote.ts
@@ -12,30 +12,27 @@ export interface UseOneInchQuoteParams {
 }
 export interface UseOneInchQuoteOptions extends UseResolveCallOptions {}
 
-export const useOneInchQuote = (
-  params: UseOneInchQuoteParams,
-  options: UseOneInchQuoteOptions = {},
-) => {
+export const useOneInchQuote = (params: UseOneInchQuoteParams, options: UseOneInchQuoteOptions = {}) => {
   const { Moralis } = useMoralis();
 
   const { fetch, data, isFetching, isLoading, error } = _useResolvePluginCall(
     Plugin.ONE_INCH,
     Moralis.Plugins?.oneInch?.quote,
     null,
-    {
-      chain: params.chain ?? DEFAULT_API_CHAIN,
-      // The token you want to swap
-      fromTokenAddress: params.fromToken.address,
-      // The token you want to receive
-      toTokenAddress: params.toToken.address,
-      amount: Moralis.Units.Token(
-        params.fromAmount,
-        params.fromToken.decimals,
-      ).toString(),
-    },
+    Object.keys(params)?.length
+      ? {
+          chain: params.chain ?? DEFAULT_API_CHAIN,
+          // The token you want to swap
+          fromTokenAddress: params.fromToken.address,
+          // The token you want to receive
+          toTokenAddress: params.toToken.address,
+          amount: Moralis.Units.Token(params.fromAmount, params.fromToken.decimals).toString(),
+        }
+      : null,
     options,
-    false,
+    false
   );
 
   return { getQuote: fetch, data, isFetching, isLoading, error };
 };
+
