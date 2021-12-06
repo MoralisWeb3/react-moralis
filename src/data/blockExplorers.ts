2de5b8c93dcebfdc05db6d8b25f7f5e3031afcaf
Dmitry Kapeliushnyi <78314301+Y0moo@users.noreply.github.com>
2021-12-06 16:54:00 +0200

    added fantom+changed mumbai explorer

--- src/data/blockExplorers.ts
+++ src/data/blockExplorers.ts
@@ -9,5 +9,6 @@ export const blockExplorers: Record<string, string | null> = {
   "0x38": "https://bscscan.com/",
   "0x61": "https://testnet.bscscan.com",
   "0x89": "https://explorer-mainnet.maticvigil.com",
-  "0x13881": "https://rpc-mumbai.matic.today",
+  "0x13881": "https://mumbai.polygonscan.com",
+  "0xfa": "https://ftmscan.com/"
 };
